dc56a847c877c8d8593b3c9c1f00a3cc0fa0506d
jodmz5 <31548323+jodmz5@users.noreply.github.com>
2018-11-28 08:51:04 -0600

    Add files via upload

--- ForRealio2.ino
+++ ForRealio2.ino
@@ -19,8 +19,6 @@ int lightval;
 int tempthresh=100;// arbitrary thresholds
 int strainthresh=8;
 int lightthresh=200;
-int command;
-
 
 
 void setup() {
@@ -30,18 +28,20 @@ void setup() {
   radio.setPALevel(RF24_PA_MIN);//sets power level to minimun. Must add capacitor to change to MAX. MAX will have longer range, but more current draw...
   radio.setDataRate(RF24_2MBPS);//quickest rate available
   radio.setChannel(1998);//Don't use something that will be a wifi name, ie MST
-  radio.openWritingPipe(addresses[1]);// will SEND to the 2nd "addresses" index, 2node.
-  radio.openReadingPipe(1,addresses[0]);//opens a reading pope 1 between the 2
-  radio.stopListening();
+  radio.openWritingPipe(addresses[0]);// will SEND to the 2nd "addresses" index, 2node.
+  radio.openReadingPipe(1,addresses[1]);//opens a reading pope 1 between the 2
+ 
   digitalWrite(relaypin, HIGH); // might be low depending on how relays work
 
 }
 
 void loop() {
+  delay(5);
+  radio.stopListening();
   typedef struct{
-  int temp=249;//analogRead(temppin);
-  int strain=8;//analogRead(strainpin);
-  int light=45;//analogRead(lightpin);
+  int temp=12;//analogRead(temppin);
+  int strain=190;//analogRead(strainpin);
+  int light=167;//analogRead(lightpin);
   }
   A_t;
   A_t data;
@@ -53,24 +53,33 @@ void loop() {
     digitalWrite(relaypin, LOW);
   }
   if(!radio.write(&data, sizeof(data))){
-    Serial.println("NOT SENT");
   }
-  
+  radio.startListening();
   if(radio.available()){
-    radio.startListening();
     while(radio.available()){
-      radio.read(&command, sizeof(int));
-      Serial.println(command);
-      if(command==120){
-        digitalWrite(relaypin,LOW);
-      }else if(command==111){
-        digitalWrite(relaypin, HIGH);
+      typedef struct{
+        int command;
       }
-      
+      B_t;
+      B_t commands;
+      radio.read(&commands, sizeof(commands));
+      Serial.println(commands.command);
+      return;
+       if(commands.command==120){
+        digitalWrite(relaypin,LOW);
+      }else if(commands.command==111){
+        digitalWrite(relaypin, HIGH);*/
     }
   }
-  radio.stopListening();
+      
 
-  }
+  
+  
+   
+     
+    }
+
+
+ 
 
 
