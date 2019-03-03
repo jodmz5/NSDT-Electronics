6dcc0386056e64d72aef4a6d4cd727a88de43b7f
jodmz5 <31548323+jodmz5@users.noreply.github.com>
2019-03-03 15:48:04 -0600

    BIG CHANGES
    Change order and logic. Also added pressure to the data.

--- ForRealio2.ino
+++ ForRealio2.ino
@@ -2,7 +2,6 @@
 #include "Arduino.h"
 #include <SPI.h>
 #include<RF24.h>
-//uint32_t test[]={6,7,8,9};
 
 //Set up on SPI and signal pins  7 and 8
 RF24 radio(7,8); //can change these two pins to any unused digital pins
@@ -10,15 +9,15 @@ RF24 radio(7,8); //can change these two pins to any unused digital pins
 // give the radios addresses
 byte addresses[][6]={"1node","2node"};
 int relaypin=4;
-int temppin=A1;
-int strainpin=A2;
-int lightpin=A5;
+int temppin=A6;
+int pressurepin=A2;
+int lightpin=A1;
 int tempval;
-int strainval;
-int lightval;
+int pressureval;
+int lightval; 
 int tempthresh=100;// arbitrary thresholds
-int strainthresh=8;
-int lightthresh=200;
+int pressurethresh=800000;
+int lightthresh=2000000;
 
 
 void setup() {
@@ -31,7 +30,7 @@ void setup() {
   radio.openWritingPipe(addresses[0]);// will SEND to the 2nd "addresses" index, 2node.
   radio.openReadingPipe(1,addresses[1]);//opens a reading pope 1 between the 2
   pinMode(relaypin,OUTPUT);
-  digitalWrite(relaypin, HIGH); // might be low depending on how relays work
+  pinMode(temppin, INPUT);
 
 }
 
@@ -39,55 +38,34 @@ void loop() {
   delay(5);
   radio.stopListening();
   typedef struct{
-  int temp=12;//analogRead(temppin);
-  int strain=190;//analogRead(strainpin);
+  int temp=10;//analogRead(temppin);
+  int pressure=190;//analogRead(strainpin);
   int light=167;//analogRead(lightpin);
   }
   A_t;
   A_t data;
-  if(data.temp>=tempthresh){
-    digitalWrite(relaypin, HIGH);//might be HIGH based on how relays work
-  }else if(data.strain>=strainthresh){
-    digitalWrite(relaypin, HIGH);
-  }else if(data.light>=lightthresh){
-    digitalWrite(relaypin, HIGH);
-  }
-  if(!radio.write(&data, sizeof(data))){
-  }
+  if(!radio.write(&data,sizeof(data))){}
   radio.startListening();
-  int wowow=10;
-  if(radio.available()){
-    
-    while(radio.available()){
-      typedef struct{
-        int command;
+  typedef struct{
+  int command;
+  }
+  B_t;
+  B_t commands;
+    if(radio.available()){
+      while(radio.available()){
+        radio.read(&commands, sizeof(commands));
       }
-      B_t;
-      B_t commands;
-      radio.read(&commands, sizeof(commands));
-      Serial.println(commands.command);
-      wowow=commands.command;
-      if(wowow>=120){//off
-        digitalWrite(relaypin,HIGH);
-      }else if(wowow==111){//on
-        digitalWrite(relaypin, LOW);
-        return;
+    }
+    if(commands.command!=111){
+      if(data.temp>=tempthresh||data.pressure>=pressurethresh||data.light>=lightthresh){
+        digitalWrite(relaypin, HIGH);//or low, whichever side we use
+      }
+      else{
+        Serial.println(commands.command);
+      }
+    }else{
+      digitalWrite(relaypin, HIGH);
+    }
     }
       
- 
-  }
-  }
-}
-  
-    
-
-  
-  
-   
-     
-    
-
-
- 
-
 
